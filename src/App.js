00dc2b316fe6813cea91ffc51747f41ee7d04254
MoPaMo <mopamo@users.noreply.github.com>
2024-12-17 10:44:25 +0100

    Refactor App component to initialize state for articles and update header to display game title

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [startArticle, setStartArticle] = useState(null);
+  const [targetArticle, setTargetArticle] = useState(null);
+  const [currentArticle, setCurrentArticle] = useState(null);
+  const [path, setPath] = useState([]);
+
+  useEffect(() => {}, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Wiki Race Game</h1>
     </div>
   );
 }
