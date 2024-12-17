44e65bddb64bf11739fda9f3b1052e2f442ae425
MoPaMo <mopamo@users.noreply.github.com>
2024-12-17 13:50:44 +0100

    Update Article component to handle link titles correctly and adjust CSS for list items

--- src/Article.jsx
+++ src/Article.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 
 function Article({ links, onLinkClick }) {
+    console.log(JSON.stringify(links));
   return (
     <div>
       <h2>Current Article Links:</h2>
       <ul>
         {links.map(
           (link, index) =>
-            link.exists && (
+            link.exists !== undefined && (
               <li key={index}>
-                <button onClick={() => onLinkClick(link.title)}>
-                  {link.title}
+                <button onClick={() => onLinkClick(link["*"])}>
+                  {link["*"]}
                 </button>
               </li>
             )
